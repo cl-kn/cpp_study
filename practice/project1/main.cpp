ea16ab4ece196b96dc65a8880772ddef420e0736
cl-kn <gitclpg@outlook.jp>
2020-12-23 14:53:08 +0900

    ANIMAL (at cl)

---
+++ practice/project1/main.cpp
@@ -0,0 +1,14 @@
+//***************************************************
+/** 12/23 */
+//***************************************************
+
+#include <iostream>
+#include "ANIMAL.h"
+
+int main(void)
+{
+    Animal animal1;
+    animal1.printAnimal();
+
+    return 0;
+}
