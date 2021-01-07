90a6145848045af338dd3d54f1235f5a8a1f2815
cl-kn <72800355+cl-kn@users.noreply.github.com>
2021-01-07 23:05:30 +0900

    void set_allValue(int id, int price, int stock);

--- b_dokushu_cpp/ch02/list_2.7/main.cpp
+++ b_dokushu_cpp/ch02/list_2.7/main.cpp
@@ -1,6 +1,6 @@
 //***************************************************
 /** 21/1/7
- * getter , setter
+ * 練習問題 2.2 getter , setter
  */
 //***************************************************
 
@@ -22,6 +22,8 @@ public:
 
     int get_stock();
     void set_stock(int stock);
+
+    void set_allValue(int id, int price, int stock);
 };
 
 int Product::get_id() //IDのgetter
@@ -41,6 +43,12 @@ int Product::get_price() //priceのgetter
 
 void Product::set_price(int price) //priseのsetter
 {
+    if (price < 0)
+    {
+        std::cout << "[price = " << price << "] illegal value..." << std::endl;
+        this->price = 0;
+        return;
+    }
     this->price = price;
 }
 
@@ -51,19 +59,62 @@ int Product::get_stock() //stockのgetter
 
 void Product::set_stock(int stock) //stockのsetter
 {
+    if (stock < 0)
+    {
+        std::cout << "[stock = " << stock << "] illegal value..." << std::endl;
+        this->stock = 0;
+        return;
+    }
+    this->stock = stock;
+}
+
+void Product::set_allValue(int id, int price, int stock)
+{
+    if (price < 0)
+    {
+        std::cout << "[price = " << price << "] illegal value..." << std::endl;
+        this->price = 0;
+        return;
+    }
+
+    if (stock < 0)
+    {
+        std::cout << "[stock = " << stock << "] illegal value..." << std::endl;
+        this->stock = 0;
+        return;
+    }
+    this->id = id;
+    this->price = price;
     this->stock = stock;
 }
 
 int main(void)
 {
+    //toyインスタンス生成
     Product toy;
     toy.set_id(1);
-    toy.set_price(2500);
-    toy.set_stock(3);
+    toy.set_price(-2500);
+    toy.set_stock(-3);
 
-    std::cout << "ID:" << toy.get_id() << std::endl;
-    std::cout << "PRICE:" << toy.get_price() << std::endl;
-    std::cout << "STOCK:" << toy.get_stock() << std::endl;
+    Product *pts = &toy;
+
+    std::cout << "<toy>" << std::endl;
+    std::cout << "id:" << pts->get_id() << std::endl;
+    std::cout << "price:" << pts->get_price() << std::endl;
+    std::cout << "stock:" << pts->get_stock() << std::endl;
+
+    std::cout << "--------------------------------------" << std::endl;
+
+    //pencil インスタンス生成
+    Product pencil;
+    pencil.set_allValue(21, 4500, 10);
+
+    Product *pts_pencil = &pencil;
+
+    std::cout << "<pencil>" << std::endl;
+    std::cout << "id:" << pts_pencil->get_id() << std::endl;
+    std::cout << "price:" << pts_pencil->get_price() << std::endl;
+    std::cout << "stock:" << pts_pencil->get_stock() << std::endl;
 
     return 0;
 }
